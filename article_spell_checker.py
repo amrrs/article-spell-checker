528c09a7faa5214c54521e8dacd2b47c098f31e2
amrrs <amrrs@outlook.com>
2017-06-05 22:34:08 +0530

    Update article_spell_checker.py

--- article_spell_checker.py
+++ article_spell_checker.py
@@ -2,17 +2,10 @@ import newspaper
 from nltk import word_tokenize
 import enchant
 import re
-#url = 'https://byrslf.co/a-smile-no-the-smile-e37af3f65b99#.2ccdxl5rp' #No Spelling Mistakes
-url = 'https://hackernoon.com/dilemmas-of-a-digital-lifestyle-27c044940157'
-
-# ### Extracting the Article Content
-
-my_article = newspaper.Article(url,language='en')
+url = 'https://hackernoon.com/dilemmas-of-a-digital-lifestyle-27c044940157' #input URL
+my_article = newspaper.Article(url,language='en') # ### Extracting the Article Content
 my_article.download()
-my_article.parse()
-# ### Printing the parsed Article Text 
-print(my_article.text)
-# ### Spell-Checking the tokenized words 
-d = enchant.Dict("en_US")
-non_dict_words = list(set([word.encode('ascii', 'ignore') for word in word_tokenize(my_article.text) if d.check(word) is False and re.match('^[a-zA-Z ]*$',word)] ))
-non_dict_words
+my_article.parse()  
+d = enchant.Dict("en_US") # ### Spell-Checking the tokenized words 
+print(list(set([word.encode('ascii', 'ignore') for word in word_tokenize(my_article.text) if d.check(word) is False and re.match('^[a-zA-Z ]*$',word)] )))
+
